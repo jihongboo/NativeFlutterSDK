859176acaa4b703b08f29acb2c803a092cc188ab
纪洪波 <hongbo.ji@bytedance.com>
2019-02-20 17:49:42 +0800

    add router

--- FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
+++ FlutterPluginRegistrant/Classes/GeneratedPluginRegistrant.m
@@ -3,10 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <hybrid_stack_manager/HybridStackManager.h>
+#import <path_provider/PathProviderPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [HybridStackManager registerWithRegistrar:[registry registrarForPlugin:@"HybridStackManager"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
 }
 
 @end
